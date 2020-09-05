8ce7462d8ef678db7bd9b2269b64cc19e21c0fb6
Jimi Damon <jdamon@gmail.com>
2020-09-05 14:47:51 -0700

    :sparkles: Adding R 4.0.0 environment

---
+++ R/4.0.0.lua
@@ -0,0 +1,13 @@
+help([[
+R 4.0.0 version 
+]])
+
+whatis("Version: 4.0.0")
+whatis("Keywords: System, Utility, Statistics")
+whatis("URL: https://github.com/open-source-parsers/jsoncpp/")
+whatis("Description: R 4.0.0 ")
+
+
+prepend_path( "PATH", os.getenv("TOOLSDIR") .. "/R-4.0.0/bin" )
+
+
